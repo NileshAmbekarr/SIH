8964e9526e17ffff545bf2d3b9d5c43c398e73a7
Setup Github Desktop <nileshambekar9281@gmail.com>
2024-09-13 22:59:58 +0530

    Data training Python code

---
+++ DeepLab_Road_Detection-main/1_Train_Data.py
@@ -0,0 +1,282 @@
+import os
+import cv2 # type: ignore
+import numpy as np # type: ignore
+import pandas as pd
+import random
+import tqdm
+import seaborn as sns
+import matplotlib.pyplot as plt
+import segmentation_models_pytorch as smp
+from segmentation_models_pytorch import utils
+import warnings
+warnings.filterwarnings("ignore")
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+import albumentations as album
+
+# Load metadata and preprocess data
+DATA_DIR = r'C:\Users\MohammadalizadehkorM\Downloads\DeepGlobe Road Extraction Dataset'
+
+metadata_df = pd.read_csv(os.path.join(DATA_DIR, 'metadata.csv'))
+metadata_df = metadata_df[metadata_df['split']=='train']
+metadata_df = metadata_df[['image_id', 'sat_image_path', 'mask_path']]
+metadata_df['sat_image_path'] = metadata_df['sat_image_path'].apply(lambda img_pth: os.path.join(DATA_DIR, img_pth))
+metadata_df['mask_path'] = metadata_df['mask_path'].apply(lambda img_pth: os.path.join(DATA_DIR, img_pth))
+# Shuffle DataFrame
+metadata_df = metadata_df.sample(frac=1).reset_index(drop=True)
+
+# Perform 90/10 split for train / val
+valid_df = metadata_df.sample(frac=0.1, random_state=42)
+train_df = metadata_df.drop(valid_df.index)
+len(train_df), len(valid_df)
+
+# Define class names and RGB values
+class_dict = pd.read_csv(os.path.join(DATA_DIR, 'class_dict.csv'))
+class_names = class_dict['name'].tolist()
+class_rgb_values = class_dict[['r','g','b']].values.tolist()
+
+# Useful to shortlist specific classes in datasets with large number of classes
+select_classes = ['background', 'road']
+
+# Get RGB values of required classes
+select_class_indices = [class_names.index(cls.lower()) for cls in select_classes]
+select_class_rgb_values =  np.array(class_rgb_values)[select_class_indices]
+
+# Define helper functions
+def visualize(**images):
+    """
+    Plot images in one row
+    """
+    n_images = len(images)
+    plt.figure(figsize=(20,8))
+    for idx, (name, image) in enumerate(images.items()):
+        plt.subplot(1, n_images, idx + 1)
+        plt.xticks([]);
+        plt.yticks([])
+        # get title from the parameter names
+        plt.title(name.replace('_',' ').title(), fontsize=20)
+        plt.imshow(image)
+    plt.show()
+
+def one_hot_encode(label, label_values):
+    """
+    Convert a segmentation image label array to one-hot format
+    by replacing each pixel value with a vector of length num_classes
+    """
+    semantic_map = []
+    for colour in label_values:
+        equality = np.equal(label, colour)
+        class_map = np.all(equality, axis=-1)
+        semantic_map.append(class_map)
+    semantic_map = np.stack(semantic_map, axis=-1)
+    return semantic_map
+
+def reverse_one_hot(image):
+    """
+    Transform a 2D array in one-hot format (depth is num_classes),
+    to a 2D array with only 1 channel, where each pixel value is
+    the classified class key.
+    """
+    x = np.argmax(image, axis=-1)
+    return x
+
+def colour_code_segmentation(image, label_values):
+    """
+    Given a 1-channel array of class keys, colour code the segmentation results.
+    """
+    colour_codes = np.array(label_values)
+    x = colour_codes[image.astype(int)]
+    return x
+
+# Define dataset class
+class RoadsDataset(torch.utils.data.Dataset):
+    def __init__(
+            self,
+            df,
+            class_rgb_values=None,
+            augmentation=None,
+            preprocessing=None,
+    ):
+        self.image_paths = df['sat_image_path'].tolist()
+        self.mask_paths = df['mask_path'].tolist()
+        self.class_rgb_values = class_rgb_values
+        self.augmentation = augmentation
+        self.preprocessing = preprocessing
+
+    def __getitem__(self, i):
+        image = cv2.cvtColor(cv2.imread(self.image_paths[i]), cv2.COLOR_BGR2RGB)
+        mask = cv2.cvtColor(cv2.imread(self.mask_paths[i]), cv2.COLOR_BGR2RGB)
+        mask = one_hot_encode(mask, self.class_rgb_values).astype('float')
+        if self.augmentation:
+            sample = self.augmentation(image=image, mask=mask)
+            image, mask = sample['image'], sample['mask']
+        if self.preprocessing:
+            sample = self.preprocessing(image=image, mask=mask)
+            image, mask = sample['image'], sample['mask']
+        return image, mask
+
+    def __len__(self):
+        return len(self.image_paths)
+
+# Define preprocessing and augmentation functions
+def get_training_augmentation():
+    train_transform = [
+        album.HorizontalFlip(p=0.5),
+        album.VerticalFlip(p=0.5),
+    ]
+    return album.Compose(train_transform)
+
+def to_tensor(x, **kwargs):
+    return x.transpose(2, 0, 1).astype('float32')
+
+def get_preprocessing(preprocessing_fn=None):
+    _transform = []
+    if preprocessing_fn:
+        _transform.append(album.Lambda(image=preprocessing_fn))
+    _transform.append(album.Lambda(image=to_tensor, mask=to_tensor))
+    return album.Compose(_transform)
+
+# Create dataset instances
+dataset = RoadsDataset(train_df, class_rgb_values=select_class_rgb_values)
+random_idx = random.randint(0, len(dataset)-1)
+image, mask = dataset[2]
+
+visualize(
+    original_image=image,
+    ground_truth_mask=colour_code_segmentation(reverse_one_hot(mask), select_class_rgb_values),
+    one_hot_encoded_mask=reverse_one_hot(mask)
+)
+
+# Create augmented dataset
+augmented_dataset = RoadsDataset(
+    train_df,
+    augmentation=get_training_augmentation(),
+    class_rgb_values=select_class_rgb_values,
+)
+
+random_idx = random.randint(0, len(augmented_dataset)-1)
+
+# Different augmentations on image/mask pairs
+for idx in range(3):
+    image, mask = augmented_dataset[idx]
+    visualize(
+        original_image=image,
+        ground_truth_mask=colour_code_segmentation(reverse_one_hot(mask), select_class_rgb_values),
+        one_hot_encoded_mask=reverse_one_hot(mask)
+    )
+
+# Define model parameters
+ENCODER = 'resnet50'
+ENCODER_WEIGHTS = 'imagenet'
+CLASSES = select_classes
+ACTIVATION = 'sigmoid'
+
+# Create segmentation model
+model = smp.DeepLabV3Plus(
+    encoder_name=ENCODER,
+    encoder_weights=ENCODER_WEIGHTS,
+    classes=len(CLASSES),
+    activation=ACTIVATION,
+)
+
+preprocessing_fn = smp.encoders.get_preprocessing_fn(ENCODER, ENCODER_WEIGHTS)
+
+# Get train and val dataset instances
+train_dataset = RoadsDataset(
+    train_df,
+    augmentation=get_training_augmentation(),
+    preprocessing=get_preprocessing(preprocessing_fn),
+    class_rgb_values=select_class_rgb_values,
+)
+
+valid_dataset = RoadsDataset(
+    valid_df,
+    preprocessing=get_preprocessing(preprocessing_fn),
+    class_rgb_values=select_class_rgb_values,
+)
+
+# Get train and val data loaders
+train_loader = DataLoader(train_dataset, batch_size=4, shuffle=True, num_workers=4)
+valid_loader = DataLoader(valid_dataset, batch_size=4, shuffle=False, num_workers=4)
+
+if __name__ == '__main__':
+    # Set flag to train the model or not
+    TRAINING = True
+    EPOCHS = 3
+    DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    loss = smp.utils.losses.DiceLoss()
+    metrics = [smp.utils.metrics.IoU(threshold=0.5)]
+    optimizer = torch.optim.Adam([dict(params=model.parameters(), lr=0.00008),])
+
+    train_epoch = smp.utils.train.TrainEpoch(
+        model,
+        loss=loss,
+        metrics=metrics,
+        optimizer=optimizer,
+        device=DEVICE,
+        verbose=True,
+    )
+
+    valid_epoch = smp.utils.train.ValidEpoch(
+        model,
+        loss=loss,
+        metrics=metrics,
+        device=DEVICE,
+        verbose=True,
+    )
+
+    if TRAINING:
+        best_iou_score = 0.0
+        train_logs_list, valid_logs_list = [], []
+
+        for i in range(0, EPOCHS):
+            print('\nEpoch: {}'.format(i))
+            train_logs = train_epoch.run(train_loader)
+            valid_logs = valid_epoch.run(valid_loader)
+            train_logs_list.append(train_logs)
+            valid_logs_list.append(valid_logs)
+
+            if best_iou_score < valid_logs['iou_score']:
+                best_iou_score = valid_logs['iou_score']
+                torch.save(model, './best_model.pth')
+                print('Model saved!')
+
+    # Perform predictions on test dataset
+    test_dataset = RoadsDataset(
+        valid_df,
+        preprocessing=get_preprocessing(preprocessing_fn),
+        class_rgb_values=select_class_rgb_values,
+    )
+
+    test_dataloader = DataLoader(test_dataset)
+
+    test_dataset_vis = RoadsDataset(
+        valid_df,
+        class_rgb_values=select_class_rgb_values,
+    )
+
+    sample_preds_folder = 'sample_predictions/'
+    if not os.path.exists(sample_preds_folder):
+        os.makedirs(sample_preds_folder)
+
+    for idx in range(len(test_dataset)):
+        image, gt_mask = test_dataset[idx]
+        image_vis = test_dataset_vis[idx][0].astype('uint8')
+        x_tensor = torch.from_numpy(image).to(DEVICE).unsqueeze(0)
+        pred_mask = model(x_tensor)
+        pred_mask = pred_mask.detach().squeeze().cpu().numpy()
+        pred_mask = np.transpose(pred_mask,(1,2,0))
+        pred_road_heatmap = pred_mask[:,:,select_classes.index('road')]
+        pred_mask = colour_code_segmentation(reverse_one_hot(pred_mask), select_class_rgb_values)
+        gt_mask = np.transpose(gt_mask,(1,2,0))
+        gt_mask = colour_code_segmentation(reverse_one_hot(gt_mask), select_class_rgb_values)
+        cv2.imwrite(os.path.join(sample_preds_folder, f"sample_pred_{idx}.png"), np.hstack([image_vis, gt_mask, pred_mask])[:,:,::-1])
+
+        visualize(
+            original_image=image_vis,
+            ground_truth_mask=gt_mask,
+            predicted_mask=pred_mask,
+            pred_road_heatmap=pred_road_heatmap
+        )
